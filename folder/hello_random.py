fc7a15b18d157282b22144c987ead7f3b3cd9383
rafaelrefaiya <rafael.refaiya@stud.th-rosenheim.de>
2026-07-14 22:10:31 +0200

    removal of files

--- folder/hello_random.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def generate_random_digits(length):
-    # Generates a string of 'length' random digits (0-9)
-    return ''.join([str(random.randint(0, 9)) for _ in range(length)])
-
-if __name__ == "__main__":
-    random_number = generate_random_digits(100)
-    print("hello" + random_number)
